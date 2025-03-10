4487233c610f1f91baa0bef66c10b55659a77096
humasae <root@T14-PF4F57JW.sdggroup.local>
2025-03-10 04:52:02 +0100

    refactor spark utilities file

--- spark_utils.py
+++ spark_utils.py
@@ -12,63 +12,109 @@ spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 root_path = "."
 
+# INPUT Functions
+
+def get_dataframe_from_file(input, file_path):
+    """
+    This function creates a new dataframe from file based on the input variable parameters.
+    It loops over spark_options to add dataframe options dinamically
+    """
+    csvdf = spark.read.format(input.config.get("format"))
+    for key,value in input.spark_options.items():
+        csvdf = csvdf.option(key, value)
+    csvdf = csvdf.load(file_path)
+    return csvdf
+
+# TRANSFORMATION Functions
+
+def add_fields_to_dataframe(df, fields):
+    """
+    This function checks if there are any literal (searching for any single quote) in fields before adding any column,
+    using lit or expr functions
+    """
+    for field in fields:
+        if "'" in field["expression"]:
+            df_new_fields = df.withColumn(field["name"], lit(field["expression"]))
+        else:
+            df_new_fields = df.withColumn(field["name"], expr(field["expression"]))
+    return df_new_fields
+
+# OUTPUT Functions
+
+def generate_output_file(df, output):
+    """
+    Generate output file, looking for any partition to be applied in output config
+    """
+    df = df.write.format(output.config["format"]) \
+    .mode(output.config["save_mode"])
+    if("partition") in output.config:
+        df = df.partitionBy(output.config["partition"])
+    df.save(f"./{output.config["path"]}")
+
+def generate_delta_table_with_merge(df, output):
+    """
+    Create table if it doesn't exist or merge with previous one
+    """
+    tmp_path = "tmp/delta-table"
+    if not DeltaTable.isDeltaTable(spark, tmp_path):
+        df.repartition(1).write.format("delta").save(tmp_path)
+    else:
+        previous_table = DeltaTable.forPath(spark, tmp_path)
+        
+        merge_keys = ""
+        for key in output.config["primary_key"]:
+            if not len(merge_keys) == 0:
+                merge_keys = merge_keys + " and "
+            merge_keys = f"{merge_keys} target.{key} = source.{key}"
+
+
+        previous_table.alias("target").merge(
+            df.alias("source"), merge_keys
+        ).execute
+
+
+
 def process_dataflows(dataflows, year):
+    """
+    This function loops over dataflows in order to:
+        - create a dataframe from file, given the input instructions
+        - apply dataframe transformations, as add fields or apply filters
+        - generate json and delta tables, from output specifications
+
+    It creates a dictionary (df_dict) to add the processed dataframes (original, columns added, filters applied)
+    """
     for dataf in dataflows:
+
         df_dict = {}
+        
         for input in dataf.inputs:
             if input.type == "file":
                 final_csv_path = root_path + f"{(input.config.get("path")).replace('{{ year }}', year)}"
-                csvdf = spark.read.format(input.config.get("format"))
-                for key,value in input.spark_options.items():
-                    csvdf = csvdf.option(key, value)
-                csvdf = csvdf.load(final_csv_path)
-
-                df_dict[input.name] = csvdf
+                df_dict[input.name] = get_dataframe_from_file(input, final_csv_path)
 
         for trans in dataf.transformations:
             if "fields" in trans.config:
                 fields = trans.config["fields"]
-                for field in fields:
-                    if "'" in field["expression"]:
-                        df_new_fields = df_dict[trans.input].withColumn(field["name"], lit(field["expression"]))
-                    else:
-                        df_new_fields = df_dict[trans.input].withColumn(field["name"], expr(field["expression"]))
-                df_dict[trans.name] = df_new_fields
+                # add a new dataframe to dataframe dictionary with the new columns
+                df_dict[trans.name] = add_fields_to_dataframe(df_dict[trans.input], fields)
 
             if "filter" in trans.config:
                 filter = trans.config["filter"]
-                for field in fields:
-                    df_new_filter = df_dict[trans.input].filter(filter)
+                # recover df with the two aditional columns previously created 
+                df_new_filter = df_dict[trans.input].filter(filter)
+                # add a new dataframe to dataframe dictionary with the filter applied
                 df_dict[trans.name] = df_new_filter
                         
         for output in dataf.outputs:
             if(output.type == "file"):
                 df = df_dict[output.input]
-                df = df.write.format(output.config["format"]) \
-                .mode(output.config["save_mode"])
-                if("partition") in output.config:
-                    df = df.partitionBy(output.config["partition"])
-                df.save(f"./{output.config["path"]}")
+                generate_output_file(df, output)
 
             elif (output.type == "delta"):
                 if output.config["save_mode"] == "merge" and ("primary_key") in output.config:
-                    
                     df = df_dict[output.input]
-                    if not DeltaTable.isDeltaTable(spark, "tmp/delta-table"):
-                        df.repartition(1).write.format("delta").save("tmp/delta-table")
-                    else:
-                        previous_table = DeltaTable.forPath(spark, "tmp/delta-table")
-                        
-                        merge_keys = ""
-                        for key in output.config["primary_key"]:
-                            if not len(merge_keys) == 0:
-                                merge_keys = merge_keys + " and "
-                            merge_keys = f"{merge_keys} target.{key} = source.{key}"
-
-
-                        previous_table.alias("target").merge(
-                            df.alias("source"), merge_keys
-                        ).execute
+                    generate_delta_table_with_merge(df, output)
+                    
                 else:
                     df = df_dict[output.input]
                     df.repartition(1).write.format("delta")\
